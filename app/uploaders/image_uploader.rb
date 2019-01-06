cd8e0f04f708975a576b7d1633166aacf46d7fde
Megumi Imada <m0e5g2u1imd@gmail.com>
2019-01-06 09:22:08 +0000

    modify20190106

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,21 +26,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   # def scale(width, height)
   #   # do something
-  process resize_to_limit: [200, 200]
+   process resize_to_limit: [300, 300]
+ 
   #end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-  process resize_to_limit: [100, 100]
-  end
+  #version :thumb do
+  #process resize_to_limit: [100, 100]
+  #end
   
 # 保存形式をJPGにする
   process :convert => 'jpg'
 
-  # jpg,jpeg,gif,pngしか受け付けない
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
 
  # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
   def filename
@@ -49,9 +46,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
